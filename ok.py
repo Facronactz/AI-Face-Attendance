89c993e8d12384da61e8163f7903bc402c2ce8fd
Facronactz <facronactz@gmail.com>
2021-12-28 01:11:46 +0700

    Init Repo

---
+++ ok.py
@@ -0,0 +1,16 @@
+import cv2
+import face_recognition
+
+cap = cv2.VideoCapture(0)
+ 
+while True:
+    success, img = cap.read()
+    #img = captureScreen()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
+    
+    cv2.imshow('Webcam',img)
+    cv2.waitKey(1)
